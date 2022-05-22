156af7c105f7dbeb92565e455fe4bf38c86797b2
Colin Jones <trptcolin@gmail.com>
2022-05-22 15:42:21 -0500

    Add top-level contexts w/ downstream consumers

--- src/index.js
+++ src/index.js
@@ -1,39 +1,97 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import axios from "axios";
 
-const queryClient = new QueryClient();
+const FilterContext = React.createContext(null);
+
+const Filters = ({ children }) => {
+  const [filters, setFilters] = React.useState({
+    repo: "trptcolin/consistency_fail",
+  });
+  return (
+    <FilterContext.Provider value={{ filters, setFilters }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+const RepoChoice = () => {
+  const { filters, setFilters } = React.useContext(FilterContext);
+  const [repo, setRepo] = React.useState(() => filters.repo);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFilters({ ...filters, repo });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="repo">Repo:</label>
+      <input
+        value={repo}
+        name="repo"
+        type="text"
+        onChange={(e) => setRepo(e.target.value)}
+      />
+      <button type="Submit">Submit</button>
+    </form>
+  );
+};
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <GithubRepoInfo
-        url={"https://api.github.com/repos/trptcolin/consistency_fail"}
-      />
-      <GithubRepoInfo
-        url={"https://api.github.com/repos/functional-koans/clojure-koans"}
-      />
-
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
+    <Filters>
+      <GithubRepoProvider>
+        <RepoChoice />
+        <GithubRepoInfo />
+      </GithubRepoProvider>
+    </Filters>
   );
 }
 
-const useGithubQuery = ({ url }) =>
-  useQuery(["githubRepo", url], () =>
-    axios.get(url).then((res) => {
-      console.log(`fetched url: ${url}`);
-      return res.data;
-    })
-  );
+const GithubRepoContext = React.createContext(null);
 
-function GithubRepoInfo({ url }) {
-  const { isLoading, error, data, isFetching } = useGithubQuery({ url });
+function GithubRepoProvider({ children }) {
+  const { filters } = React.useContext(FilterContext);
+  const [state, setState] = React.useState({ status: "init" });
+
+  React.useEffect(() => {
+    if (filters.repo === "") {
+      return;
+    }
+    const url = `https://api.github.com/repos/${filters.repo}`;
+    setState({ status: "loading" });
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(`fetched url: ${url}`);
+        setState({ status: "loaded", data: res.data });
+      })
+      .catch((error) => setState({ status: "error", error }));
+  }, [filters]);
+
+  if (!filters.repo || state.status === "init") {
+    return <div>[Enter repo info]</div>;
+  }
+
+  return (
+    <GithubRepoContext.Provider value={state}>
+      {children}
+    </GithubRepoContext.Provider>
+  );
+}
+
+function GithubRepoInfo() {
+  const state = React.useContext(GithubRepoContext);
+
+  const isLoading = state.status === "loading";
+  const error = state.status === "error" ? state.error : false;
+
   if (isLoading) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
+
+  // here we know state.status == loaded
+  const data = state.data;
+
   return (
     <div>
       <h1>{data.name}</h1>
@@ -41,7 +99,7 @@ function GithubRepoInfo({ url }) {
       <strong>👀 {data.subscribers_count}</strong>{" "}
       <strong>✨ {data.stargazers_count}</strong>{" "}
       <strong>🍴 {data.forks_count}</strong>
-      <div>{isFetching ? "Updating..." : ""}</div>
+      <div>{isLoading ? "Updating..." : ""}</div>
     </div>
   );
 }
